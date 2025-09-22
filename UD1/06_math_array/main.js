4f2f343b3cbf40096eaea79ba1cbcb3181d89be4
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-09-22 16:30:10 +0200

    Unidad 1 Ejercicio 6

--- UD1/06_math_array/main.js
+++ UD1/06_math_array/main.js
@@ -8,9 +8,35 @@
 
 function doCalculation(array) {
 
+    let suma=0;
+    let media=0;
+    let min = array[0];
+    let max = array[0];
 
+    for (let i = 0; i < array.length; i++){
+
+        suma += array[i];
+        media = suma/array.length;
+
+        if(array[i] < min){
+
+            min = array[i];
+
+        }
+
+        if(array[i] > max){
+
+            max = array[i];
+
+        }
+
+    }
+
+    console.log(`La suma es ${suma}`);
+    console.log(`La media es ${media}`);
+    console.log(`El minimo es ${min}`);
+    console.log(`El Maximo es ${max}`);
 }
-
 
 
 doCalculation([1,2,3,4])
